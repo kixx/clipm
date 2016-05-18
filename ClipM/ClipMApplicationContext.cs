72b8b7f5a36ae9bdbc95b1e75b76afbd236d1460
Kiki <kiki@staff.bluematrix.com>
2016-05-18 11:08:43 +0300

    Initial commit

---
+++ ClipM/ClipMApplicationContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipM
+{
+    class ClipMApplicationContext: ApplicationContext
+    {
+
+        private NotifyIcon notifyIcon;
+        private Config configWindow;
+
+        public ClipMApplicationContext() {
+            MenuItem configMenuItem = new MenuItem("Configuration", new EventHandler(ShowConfig));
+            MenuItem exitMenuItem = new MenuItem("Exit", new EventHandler(Exit));
+
+            this.notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.ApplicationIcon;
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[]
+            { configMenuItem, exitMenuItem });
+            notifyIcon.Visible = true;
+
+            this.configWindow = new Config();
+        }
+
+        void ShowConfig(object sender, EventArgs e)
+        {
+            if(configWindow.Visible)
+            {
+                configWindow.Activate();
+            }
+            else
+            {
+                configWindow.ShowDialog();
+            }
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+
+
+    }
+}
